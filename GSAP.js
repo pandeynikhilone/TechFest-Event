7d8675cc70cc7f5c22850532c52e55119473fd69
nikhil pandey <pandeynikhilone@gmail.com>
2024-08-23 22:26:32 +0530

    update on index.html at update1 branch

---
+++ GSAP.js
@@ -0,0 +1,21 @@
+gsap.from('.second-heading ',{
+    y:20,
+    delay:1,
+    duration:2,
+    opacity:0
+});
+
+gsap.from('.first-heading ',{
+    y:20,
+    delay:1,
+    duration:2,
+    opacity:0
+});
+
+gsap.from('a ',{
+    y:20,
+    delay:1,
+    duration:2,
+    opacity:0,
+    stagger:0.4
+});
